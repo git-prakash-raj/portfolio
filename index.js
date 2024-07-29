2649be1b586aa3970551b4237b551d813e400524
zexez <proprakash2002@gmail.com>
2024-07-29 23:07:04 +0530

    complete mode switch

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ document.querySelector("#mode").addEventListener("click", () => {
   ];
 
   const font = [
-    rootValue.getPropertyValue("--whiteFont"),
     rootValue.getPropertyValue("--blackFont"),
+    rootValue.getPropertyValue("--whiteFont"),
   ];
+
+  const trans = [
+    rootValue.getPropertyValue("--darkTransparent"),
+    rootValue.getPropertyValue("--lightTransparent"),
+  ];
+  const git = [
+    "./images/icons/github-mark.svg",
+    "./images/icons/github-mark-white.svg",
+  ];
+  const linked = [
+    "./images/icons/linkedin-light-com.svg",
+    "./images/icons/linkedin-dark-com.svg",
+  ];
+  const modeIcon = ["./images/icons/moonImg.svg", "./images/icons/sunImg.svg"];
   current = (current + 1) % 2;
   root.style.setProperty("--theme", theme[current]);
   root.style.setProperty("--primaryFont", font[current]);
+  root.style.setProperty("--transparentBackground", trans[current]);
+  document.querySelector("#git-hub").src = git[current];
+  document.querySelector("#linked-in").src = linked[current];
+  document.querySelector("#toggle").src = modeIcon[current];
+  console.log(document.querySelector("#toggle").src);
 });
